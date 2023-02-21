5eefaa5f2731d25900e024916d0d052dcca6cce2
rebeloper <therebeloper@gmail.com>
2023-02-21 23:42:53 +0200

    license

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "kindaSwiftUI247",
+    platforms: [
+        .iOS(.v16), .macOS(.v13), .macCatalyst(.v16), .tvOS(.v16), .watchOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "kindaSwiftUI247",
-            dependencies: []),
-        .testTarget(
-            name: "kindaSwiftUI247Tests",
-            dependencies: ["kindaSwiftUI247"]),
+            path: "./Sources/kindaSwiftUI247.xcframework")
     ]
 )
--- Sources/kindaSwiftUI247/kindaSwiftUI247.swift
+++
@@ -1,6 +0,0 @@
-public struct kindaSwiftUI247 {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
